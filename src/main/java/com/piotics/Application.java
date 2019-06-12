ed3fef11e69dca38ff281a19e45cfc27f0bbb496
Vishnu manohar <vishnumanohar7@gmail.com>
2019-06-12 18:42:38 +0530

    added servlet initialiser

--- src/main/java/com/piotics/Application.java
+++ src/main/java/com/piotics/Application.java
@@ -3,6 +3,7 @@ package com.piotics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableAutoConfiguration
 @EnableAsync
 @ComponentScan(basePackages = {"com.piotics"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
