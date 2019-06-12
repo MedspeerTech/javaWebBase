52c3ce26060b75dc6f5543693a5ca4bc63e27e63
Vishnu manohar <vishnumanohar7@gmail.com>
2019-06-12 15:35:30 +0530

    test loading

--- src/main/java/com/piotics/Application.java
+++ src/main/java/com/piotics/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableAsync	
+@EnableAsync
+@ComponentScan(basePackages = {"com.piotics"})
 public class Application {
 
 	public static void main(String[] args) {
