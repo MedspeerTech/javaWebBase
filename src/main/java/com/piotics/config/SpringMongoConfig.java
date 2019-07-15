66c27fc4d90bf7b0715a52d2ada0d1da4ae7e7cb
Vishnu manohar <vishnumanohar7@gmail.com>
2019-07-15 12:48:53 +0530

    Updated Mongo Configuration

--- src/main/java/com/piotics/config/SpringMongoConfig.java
+++ src/main/java/com/piotics/config/SpringMongoConfig.java
@@ -1,13 +1,18 @@
 package com.piotics.config;
 
 import com.mongodb.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 import java.util.Arrays;
 
@@ -28,6 +33,9 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
 
 	@Value("${mongo.password}")
 	String password;
+
+	@Autowired
+	private ApplicationContext appContext;
 
 	@Override
 	protected String getDatabaseName() {
@@ -63,9 +71,19 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
 	}
 
 	public @Bean MongoTemplate mongoTemplate() throws Exception {
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
-//	        mongoTemplate.setWriteResultChecking(WriteResultChecking.EXCEPTION);
-		return mongoTemplate;
+//		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+////	        mongoTemplate.setWriteResultChecking(WriteResultChecking.EXCEPTION);
+//		return mongoTemplate;
+		final MongoDbFactory factory = mongoDbFactory();
+
+		final MongoMappingContext mongoMappingContext = new MongoMappingContext();
+		mongoMappingContext.setApplicationContext(appContext);
+
+		// Learned from web, prevents Spring from including the _class attribute
+		final MappingMongoConverter converter = new MappingMongoConverter(factory, mongoMappingContext);
+		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+		return new MongoTemplate(factory, converter);
 	}
 
 	@Override
