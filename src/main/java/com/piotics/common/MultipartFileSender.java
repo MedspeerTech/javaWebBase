e7b55a3cb1043adefc29fb5872cd8cd08f9dc590
dijofrancis <dijofrancis01@gmail.com>
2019-10-31 16:27:29 +0530

    added configuration for logging

--- src/main/java/com/piotics/common/MultipartFileSender.java
+++ src/main/java/com/piotics/common/MultipartFileSender.java
@@ -21,8 +21,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MultipartFileSender {
 
